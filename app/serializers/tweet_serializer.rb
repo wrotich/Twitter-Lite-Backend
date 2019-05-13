75d9c54bcd7b5f6bb5657c6bad8dc79838b4d5b5
maxwellgithinji <maxwellgithinji@gmail.com>
2019-05-13 17:42:18 +0300

    bg(fixDisplayErrors): - Ensure the username is provided in the JSON reponse - Ensure while fetching the tweets, they are aranged by updated-at in descending order [Finishes #165972755]

--- app/serializers/tweet_serializer.rb
+++ app/serializers/tweet_serializer.rb
@@ -15,9 +15,17 @@ class TweetSerializer < ActiveModel::Serializer
     :body,
     :created_by,
     :created_at,
-    :tags
+    :tags,
+    :curr_user
 
   def created_at
     TimeDiffCalc.call(object.created_at)
+  end
+
+  def curr_user
+    curr_user = User.select(:name).where(
+      id: object.created_by.first
+        ).pluck(:name)[0]
+    curr_user
   end
 end
